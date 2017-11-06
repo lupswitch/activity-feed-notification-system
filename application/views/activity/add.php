6714420dd11b02758afabbc903205b093d9dc991
Yash Desai <yash@codebox.in>
2017-11-06 17:03:09 +0530

    Completed add / edit system activity text based on pre-defined and custom tags

--- application/views/activity/add.php
+++ application/views/activity/add.php
@@ -8,14 +8,24 @@
 <div class="container">
 	<form method="post">
 		<div class="form-group">
-			<label for="placeName">Slug</label>
+			<label for="activitySlug">Slug</label>
 			<input type="text" class="form-control" name="slug" id="activitySlug" aria-describedby="slugHelp" placeholder="Enter a unique slug" value="<?php echo $this->input->post('slug'); ?>">
 			<small id="slugHelp" class="form-text text-muted">Give it a unique name / slug in capitals so it can be used in code easily e.g NEW_MESSAGE</small>
 			<?php echo form_error("slug"); ?>
 		</div>
 		<div class="form-group">
-			<label for="placeName">Text</label>
+			<label for="activityText">Text</label>
+			<div class="jumbotron p-2 mb-3">
+				<p class="mb-1 lead">Available tags</p>
+				<span class="badge badge-dark p1">{sender-first-name}</span>
+				<span class="badge badge-dark p1">{sender-last-name}</span>
+				<span class="badge badge-dark p1">{sender-full-name}</span>
+				<span class="badge badge-dark p1">{receipient-first-name}</span>
+				<span class="badge badge-dark p1">{receipient-last-name}</span>
+				<span class="badge badge-dark p1">{receipient-full-name}</span>
+			</div>
 			<textarea class="form-control" name="text" id="activityText" aria-describedby="slugHelp" placeholder="Enter activity text / message"><?php echo $this->input->post('text'); ?></textarea>
+			<small id="textHelp" class="form-text text-muted">Type activity text using pre-defined tags or writing custom tags in the format <code>{custom-tag}</code> and sending it along with <code>other_activity_data</code> while calling the create activity function</small>
 			<?php echo form_error("text"); ?>
 		</div>
   		<button type="submit" class="btn btn-primary">Add</button>
